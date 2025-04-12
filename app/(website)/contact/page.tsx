b7b5b7e71085a6bd0d4cf93db26de7bc44091bdb
BradleyDev8 <bradley@hrav.io>
2025-04-12 09:26:29 +0100

    Removed instagram icon import

--- app/(website)/contact/page.tsx
+++ app/(website)/contact/page.tsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faXTwitter,
   faLinkedin,
-  faInstagram,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
