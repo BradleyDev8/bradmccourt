d2f3e0580c63ae6903561b58f8f74f29b6d03166
BradleyDev8 <bradley@opinly.ai>
2025-07-11 17:41:58 +0100

    Added project page

--- app/(website)/menu-items.ts
+++ app/(website)/menu-items.ts
@@ -1,5 +1,6 @@
 export const menuItems: { href: string; name: string; new?: boolean }[] = [
   { href: "/", name: "About" },
   { href: "/resume", name: "Resume" },
+  { href: "/project", name: "Projects" },
   { href: "/blog", name: "Blog" },
 ];
